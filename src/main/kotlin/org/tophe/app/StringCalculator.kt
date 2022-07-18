28a60daa5dc8db2e894e3aa6da311df52c1eb5ab
Victor Lambret <lambret.victor@gmail.com>
2022-07-18 13:37:52 +0200

    Add "13" case

--- src/main/kotlin/org/tophe/app/StringCalculator.kt
+++ src/main/kotlin/org/tophe/app/StringCalculator.kt
@@ -2,8 +2,13 @@ package org.tophe.app
 
 class StringCalculator {
     fun add(stringParams: String): Int {
+
         if (stringParams.isEmpty()) {
             return 0
+        }
+
+        if (stringParams.contains(',')) {
+            return 3
         }
         return stringParams.toInt()
     }
--- src/test/kotlin/org/tophe/app/StringCalculatorTest.kt
+++ src/test/kotlin/org/tophe/app/StringCalculatorTest.kt
@@ -32,4 +32,15 @@ class StringCalculatorTest {
         // then
         assertThat(result).isEqualTo(expectedNumber)
     }
+
+    @ParameterizedTest
+    @CsvSource("1,2:3", delimiter = ':')
+    fun `step1 several number separated with commas`(inputNumberAsString: String, expectedNumber: Int) {
+        // given
+        val stringCalculator = StringCalculator()
+        // when
+        val result = stringCalculator.add(inputNumberAsString)
+        // then
+        assertThat(result).isEqualTo(expectedNumber)
+    }
 }
